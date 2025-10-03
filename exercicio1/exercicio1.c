98d162cfd6e1decc67bf93810e19c13e588e327a
Eduferruzzi <eduferruzzi@gmail.com>
2025-10-02 21:51:28 -0300

    comentários 1 e 2

--- exercicio1/exercicio1.c
+++ exercicio1/exercicio1.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 
+// Declararação das funções
 void show_menu();
 void get_option(int *option);
 void get_numbers(float *n1, float *n2);
@@ -11,6 +12,8 @@ float mul(float n1, float n2);
 float div(float n1, float n2);
 void show_result(float result);
 
+// Implementação das funções
+// Função para mostrar o menu de opções
 void show_menu(){
     printf("\n=== CALCULADORA ===\n");
     printf("1. Somar\n");
@@ -20,11 +23,13 @@ void show_menu(){
     printf("0. Sair\n");
 }
 
+// Função para pegar a opção escolhida pelo usuário
 void get_option(int *option){
     printf("Selecione uma opcao: ");
     scanf("%d", option);
 }
 
+// Função para pegar os números escolhidos pelo usuário
 void get_numbers(float *n1, float *n2){
     printf("Digite o primeiro numero: ");
     scanf("%f", n1);
@@ -32,20 +37,25 @@ void get_numbers(float *n1, float *n2){
     scanf("%f", n2);
 }
 
+// Função que realiza a soma
 float sum(float n1, float n2){
     
     return n1 + n2;
 }
 
+// Função que realiza a subtração
 float sub(float n1, float n2){
     return n1 - n2;
 }
 
+// Função que realiza a multiplicação
 float mul(float n1, float n2){
     return n1 * n2;
 }
 
+// Função que realiza a divisão
 float div(float n1, float n2){
+    // Valida o valor de n2 para não ocorrer divisão por zero
     if (n2 == 0){
         printf("Nao e possivel dividir por zero, o resultado sera 0\n");
         return 0;
@@ -53,42 +63,46 @@ float div(float n1, float n2){
     return n1 / n2;
 }
 
+// Função que mostra o resultado
 void show_result(float result){
     printf("Resultado: %.2f\n", result);
 }
 
 int main(){
+    // Inicializa as variáveis usadas
     int option;
     float n1, n2, result;
 
     do {
-        show_menu();
-        get_option(&option);
+        show_menu(); // Mostra o menu
+        get_option(&option); // Pega a opção
+ 
+        // Procede conforme a opção
         switch(option) {
-            case 1:
+            case 1: // Adição
                 get_numbers(&n1, &n2);
                 result = sum(n1, n2);
                 show_result(result);
                 break;
-            case 2:
+            case 2: // Subtração
                 get_numbers(&n1, &n2);
                 result = sub(n1, n2);
                 show_result(result);
                 break;
-            case 3:
+            case 3: // Multiplicação
                 get_numbers(&n1, &n2);
                 result = mul(n1, n2);
                 show_result(result);
                 break;
-            case 4:
+            case 4: // Divisão
                 get_numbers(&n1, &n2);
                 result = div(n1, n2);
                 show_result(result);
                 break;
-            case 0:
+            case 0: // Sai do ciclo
                 printf("Saindo...");
                 break;
-            default:
+            default: // Pede pro usuário repetir, caso não seja uma das opções válidas
                 printf("Opcao invalida tente novamente\n");
         }
     } while (option);
--- exercicio2/exercicio2.c
+++ exercicio2/exercicio2.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 
+// Declararação das funções
 void fill_array(int *array, int size);
 void show_array(const int array[], const int size, const int is_ordered);
 int max(int array[], int size);
@@ -9,6 +10,8 @@ int min(int array[], int size);
 float avg(int array[], int size);
 void order(int *array, int size, int *is_ordered);
 
+// Implementação das funções
+// Pega os inteiros do usuário
 void fill_array(int *array, int size){
     printf("Digite %d numeros: \n", size);
     for (int i = 0; i < size; i++) {
@@ -17,18 +20,23 @@ void fill_array(int *array, int size){
     }
 }
 
+// Mostra o array
 void show_array(const int array[], const int size, const int is_ordered){
+    // Se não estiver ordenado fala que é o original
     if (!is_ordered) {
         printf("Array original: [");
-    } else {
+    } else { // Caso o contrário fala que é o ordenado
         printf("Array ordenado: [");
     }
+    // Faz o padrão "n, " até o penúltimo elemento
     for (int i = 0; i < size-1; i++) {
         printf("%d, ", array[i]);
     }
+    // Fecha o array "n]"
     printf("%d]\n", array[size-1]);
 }
 
+// Função que retorna o maior elemento do array
 int max(int array[], int size){
     int max = array[0];
     for (int i = 0; i < size; i++) {
@@ -39,6 +47,7 @@ int max(int array[], int size){
     return max;
 }
 
+// Função que retorna o menor elemento do array
 int min(int array[], int size){
     int min = array[0];
     for (int i = 0; i < size; i++) {
@@ -49,6 +58,7 @@ int min(int array[], int size){
     return min;
 }
 
+// Função que retorna a média dos valores do array
 float avg(int array[], int size){
     int sum = 0;
     for (int i = 0; i < size; i++) {
@@ -57,6 +67,7 @@ float avg(int array[], int size){
     return (float)sum/size;
 }
 
+// Função que ordena o array ordenado utilizando método bubblesort 
 void order(int *array, int size, int *is_ordered){
     int aux;
     for (int i = 0; i < size - 1; i++) {
@@ -72,19 +83,20 @@ void order(int *array, int size, int *is_ordered){
 }
 
 int main(){
+    // Inicializa as variáveis
     int array[10];
-    int size = sizeof(array) / sizeof(array[0]);
+    int size = sizeof(array) / sizeof(array[0]); // Faz o tamanho do array em bytes dividido pelo tamanha de um elemento para achar o tamanho dele
     int is_ordered = 0;
 
-    fill_array(array, size);
-    show_array(array, size, is_ordered);
+    fill_array(array, size); // Preenche o array com o input
+    show_array(array, size, is_ordered); // Mostra o array original
 
-    printf("Maior valor: %d\n", max(array, size));
-    printf("Menor valor: %d\n", min(array, size));
-    printf("Media: %.2f\n", avg(array, size));
+    printf("Maior valor: %d\n", max(array, size)); // Mostra o maior elemento
+    printf("Menor valor: %d\n", min(array, size)); // Mostra o menor elemento
+    printf("Media: %.2f\n", avg(array, size)); // Mostra a média
 
-    order(array, size, &is_ordered);
-    show_array(array, size, is_ordered);
+    order(array, size, &is_ordered); // Ordena o array
+    show_array(array, size, is_ordered); // Mostra o array ordenado
     
     return 0;
 }
